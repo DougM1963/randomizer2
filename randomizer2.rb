e47dd297b5fee3c064b52fa7475bb8c86c1f275b
dougm1963 <dougmeador1963@gmail.com>
2017-11-30 17:44:10 -0500

    Finished randomizer of array

---
+++ randomizer2.rb
@@ -0,0 +1,12 @@
+ Array = ["Tyler", "Christi", "Jim", "Chris", "Doug"].shuffle # This line assigns the elements to the array
+
+group1 = Array.first(3) # This line assigns the first 3 from the original array to the array group1
+group2 = Array.last(2) # This line assigns the last 2 from the original array to the array group2
+
+print group1 # This line prints out the array group1
+print group2 # This line prints out the array group2
+
+
+
+
+
